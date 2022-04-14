79a00b6e29a8b4117b9e90555ea2878867dedd0a
unknown <meshaal.nasor10@gmail.com>
2022-04-15 03:32:21 +0530

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div id="footer" className="w-full py-8">
       <div className="container">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 border-b border-gray-600">
-          <div className="col-span-2 md:col-span-4 lg:col-span-2">
+          <div className="col-span-2 md:col-span-4 lg:col-span-2 p-4">
             <h2 className="logo">Data.</h2>
             <p className="my-4">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
